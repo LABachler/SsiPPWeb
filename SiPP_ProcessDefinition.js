6f196a06581355d04a1f516aa7831c6baf45bdba
NaidaCiric <naidaciric@bulme.at>
2021-12-04 18:25:06 +0100

    Sub Menu for saved processes
    filling a list with saved processes (onlick function not yet implemented)

--- SiPP_ProcessDefinition.js
+++ SiPP_ProcessDefinition.js
@@ -17,7 +17,7 @@ parser.parseString(xml_string, function(error, result) {
     }
 });
 */
-var xmlFile = 'SiPP_modules.xml';
+var xmlFile = 'SiPP_Processes.xml';
 
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
@@ -31,51 +31,63 @@ function loadDoc() {
     };
 
 }
-/*
-function myFunction(parameter) {
-    let x = document.createElement("INPUT");
-    x.setAttribute("type", "text");
-    x.setAttribute("value", parameter.type);
-    document.body.appendChild(x);
-    return x;
-}*/
-function makeTextArea(value) {
-    let ta = document.createElement('TEXTAREA');
-    ta.innerHTML = value;
-    ta.value = value;
-    ta.className = 'textAreas';
-    ta.id = value;
-    ta.rows = 4;
-    ta.cols = 40;
-    ta.disabled = true;
-    ta.style = "overflow:auto;resize:none";
-    //ta.addEventListener('click', myFunction(param));
-    document.body.appendChild(ta);
-    return ta;
-}
+//toggle sidebar function
+$(document).ready(function () {
+    $('#homeSubmenu').on('click', function () {
+        $('#data-toggle').toggleClass('active');
+    });
+});
+
 function xmlFunction(xml) {
     let parser = new DOMParser();
     let xmlDoc = parser.parseFromString(xml, "text/xml");
-    let x = xmlDoc.getElementsByTagName("module");
-    for (let elem of x) {
+    let processName = xmlDoc.getElementsByTagName("process"); //array of all processes
+    let processModules = xmlDoc.getElementsByTagName("module_instance"); // array of all module instances
+    let processList = document.getElementById("pageSubmenu");  //list of all saved processes
 
-        let table = document.getElementById("myTableData");
-        let subTable = document.getElementById("mySubTableData")
-        let rowCount = table.rows.length;
-        let subTableRowCount = subTable.rows.length;
-        let row = table.insertRow(rowCount);
-        let nextRow = table.insertRow(rowCount+1);
-        let subRow = subTable.insertRow(subTableRowCount);
-        //let text = elem.getAttribute('name') + "\n      " +elem.childNodes.item(0).nextSibling.getAttribute('name') +
-        //  "       " + elem.childNodes.item(0).nextSibling.getAttribute('engineering_unit');
-        //row.setAttribute("colspan", "2");
-        //row.insertCell(0).firstChild = makeTextArea(text);
+    //console.log(processName.getAttribute("name"));
+    for(let process of processName){
+        let listItem = document.createElement("li");
+        let paramItem = document.createElement("a");
+        paramItem.setAttribute("href", "#");
+        let paramText = document.createTextNode(process.getAttribute("name"));
+        paramItem.appendChild(paramText);
+        listItem.appendChild(paramItem);
+        processList.appendChild(listItem);
+    }
+
+    for (let elem of processModules) {
+
+        //Main Table
+        let tblHistoryData = document.getElementById("tblHistoryData");
+        let row = tblHistoryData.insertRow(-1);
+
+
+
 
         //inserting values in the tables
-        row.insertCell(0).innerHTML = elem.getAttribute('name');
-        subRow.insertCell(0).innerHTML = elem.childNodes.item(0).nextSibling.getAttribute('name');
-        subRow.insertCell(1).innerHTML = elem.childNodes.item(0).nextSibling.getAttribute('engineering_unit');
-        nextRow.insertCell(0).innerHTML = "↓";
+        row.insertCell(-1).innerHTML = tblHistoryData.rows.length.toString();
+        row.insertCell(-1).innerHTML = elem.getAttribute('type');
+
+        let parameters = elem.getElementsByTagName("param");
+
+        for (let param of parameters){
+            //Sub Table
+            let subTable = document.createElement("table");
+            let subRow = subTable.insertRow(-1);
+            subTable.setAttribute("class", "parameters")
+            subRow.insertCell(-1).innerHTML = param.getAttribute('name');
+            subRow.insertCell(-1);
+            subRow = subTable.insertRow(-1);
+            subRow.insertCell(-1).innerHTML = param.textContent;
+            subRow.insertCell(-1).innerHTML = param.getAttribute('engineering_unit');
+           // subRow.insertCell(-1);
+            subRow = subTable.insertRow(-1);
+            let cell = document.createElement("td");
+            cell.appendChild(subTable);
+            row.appendChild(cell);
+
+        }
     }
 }
 loadDoc();
