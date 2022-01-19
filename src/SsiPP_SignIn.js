f7106a5a1d351d1af2701e1247ab623dbdeb9cf3
NaidaCiric <naidaciric@bulme.at>
2022-01-19 10:55:28 +0100

    Sign in page added (test data email: naida@gmail.com pass:12345)

---
+++ src/SsiPP_SignIn.js
@@ -0,0 +1,21 @@
+let attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    let username = document.getElementById("floatingInput").value;
+    let password = document.getElementById("floatingPassword").value;
+    if ( username === "naida@gmail.com" && password === "12345"){
+        document.location.href = "SsiPP.html"
+        alert ("Login successfully");
+    }
+    else{
+        attempt --;// Decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+        if( attempt === 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
